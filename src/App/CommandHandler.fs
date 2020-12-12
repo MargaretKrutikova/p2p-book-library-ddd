7e81f59548ea7bc4d826bbddf56563281dd92f99
Margarita Krutikova <margaretkru@gmail.com>
2020-12-12 18:13:37 +0100

    Add in memory persistence and command handler

---
+++ src/App/CommandHandler.fs
@@ -0,0 +1,16 @@
+module CommandHandler
+
+open BookListing
+open InMemoryPersistence
+
+let handleCreateBookListing (persistence: InMemoryPersistence) command =
+  Implementation.createBookListing persistence.GetUserById persistence.CreateListing command
+
+let handleBorrowBook (persistence: InMemoryPersistence) command =
+  Implementation.borrowBook persistence.GetListingById command
+
+let commandHandler (persistence: InMemoryPersistence) (command: Domain.Commands.BookListingCommand) =
+    match command with
+    | Domain.Commands.CreateBookListing data -> handleCreateBookListing persistence data
+    | Domain.Commands.BorrowBook data -> handleBorrowBook persistence data
+  
---
+++ src/App/InMemoryPersistence.fs
@@ -0,0 +1,44 @@
+module InMemoryPersistence
+
+open System
+open BookListing
+open System.Threading.Tasks
+
+type InMemoryPersistence () =
+  let mutable users: Persistence.Queries.User list = List.empty
+  let mutable listings: Persistence.Queries.Listing list = List.empty
+
+  member __.GetUserListings: Persistence.Queries.GetUserListings =
+    fun userId ->
+      listings 
+      |> Seq.filter (fun listing -> listing.UserId = userId)
+      |> Task.FromResult
+
+  member __.GetUserById: Persistence.Queries.GetUserById =
+    fun userId ->
+      users 
+      |> Seq.filter (fun user -> user.Id = userId)
+      |> Seq.head
+      |> Ok
+      |> Task.FromResult
+
+  member __.GetListingById: Persistence.Queries.GetListingById =
+    fun listingId ->
+      listings 
+      |> Seq.filter (fun listing -> listing.ListingId = listingId)
+      |> Seq.head
+      |> Ok
+      |> Task.FromResult
+
+  member __.CreateListing: Persistence.Commands.CreateListing =
+    fun model ->
+      let listing: Persistence.Queries.Listing = {
+        ListingId = model.ListingId 
+        UserId = model.UserId
+        Author = model.Author
+        Title = model.Title
+        Status = model.InitialStatus
+        PublishedDate = DateTime.UtcNow
+      }
+      listings <- listing::listings
+      Task.FromResult ()
