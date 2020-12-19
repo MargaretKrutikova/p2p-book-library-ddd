2fcac4fcb7f0fefd946ec09b1bd6874ad39c5952
Margarita Krutikova <margaretkru@gmail.com>
2020-12-20 00:41:17 +0100

    Add router to client project

---
+++ src/Client/src/Router.fs
@@ -0,0 +1,26 @@
+module App.Router
+
+[<RequireQualifiedAccess>]
+type Route =
+  | Home
+  | SignUp
+  | SignIn
+  | AllBookListings
+  | MyBookListings
+  | NotFound
+
+let parseUrl = function
+  | [ ] -> Route.Home
+  | [ "sign-in" ] -> Route.SignIn
+  | [ "sign-up" ] -> Route.SignUp
+  | [ "listings" ] -> Route.AllBookListings
+  | [ "my-pages" ] -> Route.MyBookListings
+  | _ -> Route.NotFound
+
+let urlToRoute = function
+  | Route.Home -> ""
+  | Route.SignIn -> "sign-in"
+  | Route.SignUp -> "sign-up"
+  | Route.AllBookListings -> "listings"
+  | Route.MyBookListings -> "my-pages"
+  | _ -> ""
