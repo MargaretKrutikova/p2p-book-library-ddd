5175696065c2b632cbf173608da08f4e55e38353
Margarita Krutikova <margaretkru@gmail.com>
2021-01-19 23:10:22 +0100

    Add listing projection

--- src/Core/Events.fs
+++ src/Core/Events.fs
@@ -1,34 +1,32 @@
 module Core.Events
 
-open System
 open Core.Domain.Types
 
-type ListingRequestedToBorrowEventArgs =
-    { ListingId: ListingId
-      RequesterId: UserId }
-
-type RequestToBorrowCancelledEventArgs =
-    { ListingId: ListingId
-      RequesterId: UserId }
-
-type RequestToBorrowApprovedEventArgs =
-    { ListingId: ListingId
-      BorrowerId: UserId }
-
-type ListingReturnedEventArgs =
-    { ListingId: ListingId
-      BorrowerId: UserId }
-
-type BookListingPublishedEventArgs = {
-    Listing: BookListing
-}
+type ListingRequestedToBorrowEventArgs = { RequesterId: UserId }
+type RequestToBorrowCancelledEventArgs = { RequesterId: UserId }
+type RequestToBorrowApprovedEventArgs = { BorrowerId: UserId }
+type ListingReturnedEventArgs = { BorrowerId: UserId }
+type BookListingPublishedEventArgs = { Listing: BookListing }
 
 [<RequireQualifiedAccess>]
-type Event =
+type DomainEvent =
     | BookListingPublished of BookListingPublishedEventArgs
     | ListingRequestedToBorrow of ListingRequestedToBorrowEventArgs
     | RequestToBorrowCancelled of RequestToBorrowCancelledEventArgs
     | RequestToBorrowApproved of RequestToBorrowApprovedEventArgs
     | ListingReturned of ListingReturnedEventArgs
 
-type EventEnvelope = { Timestamp: DateTime; Event: Event }
+module Projections =
+    let private applyEventToExistingListing (listing: BookListing) (event: DomainEvent): BookListing =
+        match event with
+        | DomainEvent.ListingRequestedToBorrow args -> { listing with Status = RequestedToBorrow args.RequesterId }
+        | DomainEvent.RequestToBorrowCancelled _ -> { listing with Status = Available }
+        | DomainEvent.RequestToBorrowApproved args -> { listing with Status = Borrowed args.BorrowerId }
+        | DomainEvent.ListingReturned _ -> { listing with Status = Available }
+        | _ -> listing
+
+    let applyEvent (listingOption: BookListing option) (event: DomainEvent): BookListing option =
+        match listingOption, event with
+        | None, DomainEvent.BookListingPublished args -> args.Listing |> Some
+        | Some listing, event -> applyEventToExistingListing listing event |> Some
+        | _ -> None
